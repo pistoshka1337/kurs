e3bbee7c909287e73cad7719291ef0a87e971d00
pistoshka <shvechikov.leh@mail.ru>
2025-05-11 20:26:41 +1000

    Update isAdmin.js

--- server/middleware/isAdmin.js
+++ server/middleware/isAdmin.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if (req.user && req.user.isAdmin) {
-      return next();  // Если пользователь администратор, пропускаем
+      return next();  
     }
-    return res.status(403).json({ message: 'Нет доступа' });  // Если нет, возвращаем ошибку
+    return res.status(403).json({ message: 'Нет доступа' });  
   };
